88536cf35e9353696b885d520dae93e5e77d7ece
nickratt <nickrattanachane@gmail.com>
2019-05-17 15:08:31 -0400

    Create headers.cpp

---
+++ headers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include "headers.h"
+
+using namespace std;
+
+int addition(int a, int b)
+{
+int sum;
+
+sum = a + b;
+return sum;
+
+}
+
+int subtraction(int a, int b)
+{
+  int diff;
+
+  diff = a - b;
+  return diff;
+
+}
+
+int multiplication(int a, int b)
+{
+  int mult;
+  mult = a * b;
+  return mult;
+  
+}
+
+int division(int a, int b)
+{
+  int div;
+  div = a / b;
+  return div;
+  
+}
